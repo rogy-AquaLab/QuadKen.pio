9f07e68af524bc0eb7c07055fdf21118deb56846
Fin79126 <tackygle@gmail.com>
2025-07-15 18:03:22 +0900

    servo pin 変更

--- examples/servo/main.cpp
+++ examples/servo/main.cpp
@@ -1,15 +1,31 @@
 # include <ESP32Servo.h>
 # include <Arduino.h>
 
-constexpr int Pin1 = 27; // GPIO pin for the servo
-constexpr int Pin2 = 32; // GPIO pin for the servo
+constexpr int Pin1 = 23; // GPIO pin for the servo
+constexpr int Pin2 = 22; // GPIO pin for the servo
 constexpr int Pin3 = 21; // GPIO pin for the servo
 constexpr int Pin4 = 19; // GPIO pin for the servo
+constexpr int Pin5 = 18; // GPIO pin for the servo
+constexpr int Pin6 = 32; // GPIO pin for the servo
+constexpr int Pin7 = 25; // GPIO pin for the servo
+constexpr int Pin8 = 5; // GPIO pin for the servo
+constexpr int Pin9 = 26; // GPIO pin for the servo
+constexpr int Pin10 = 27; // GPIO pin for the servo
+constexpr int Pin11 = 14; // GPIO pin for the servo
+constexpr int Pin12 = 13; // GPIO pin for the servo
 
 Servo servo1;
 Servo servo2;
 Servo servo3;
 Servo servo4;
+Servo servo5;
+Servo servo6;
+Servo servo7;
+Servo servo8;
+Servo servo9;
+Servo servo10;
+Servo servo11;
+Servo servo12;
 
 int input() {
   if (Serial.available() > 0) {
@@ -45,6 +61,15 @@ void setup() {
 	ESP32PWM::allocateTimer(1);
 	ESP32PWM::allocateTimer(2);
 	ESP32PWM::allocateTimer(3);
+	ESP32PWM::allocateTimer(4);
+	ESP32PWM::allocateTimer(5);
+	ESP32PWM::allocateTimer(6);
+	ESP32PWM::allocateTimer(7);
+	ESP32PWM::allocateTimer(8);
+	ESP32PWM::allocateTimer(9);
+	ESP32PWM::allocateTimer(10);
+	ESP32PWM::allocateTimer(11);
+	ESP32PWM::allocateTimer(12);
 
   //セットアップ
   servo1.setPeriodHertz(50);// Standard 50hz servo
@@ -55,6 +80,22 @@ void setup() {
   servo3.attach(Pin3, 500, 2400);
   servo4.setPeriodHertz(50);// Standard 50hz servo
   servo4.attach(Pin4, 500, 2400);
+  servo5.setPeriodHertz(50);// Standard 50hz servo
+  servo5.attach(Pin5, 500, 2400);
+  servo6.setPeriodHertz(50);// Standard 50hz servo
+  servo6.attach(Pin6, 500, 2400);
+  servo7.setPeriodHertz(50);// Standard 50hz servo
+  servo7.attach(Pin7, 500, 2400);
+  servo8.setPeriodHertz(50);// Standard 50hz servo
+  servo8.attach(Pin8, 500, 2400);
+  servo9.setPeriodHertz(50);// Standard 50hz servo
+  servo9.attach(Pin9, 500, 2400);
+  servo10.setPeriodHertz(50);// Standard 50hz servo
+  servo10.attach(Pin10, 500, 2400);
+  servo11.setPeriodHertz(50);// Standard 50hz servo
+  servo11.attach(Pin11, 500, 2400);
+  servo12.setPeriodHertz(50);// Standard 50hz servo
+  servo12.attach(Pin12, 500, 2400);
 }
 
 void loop() {
@@ -73,9 +114,17 @@ void loop() {
   // Serial.print("val3: ");
   // Serial.println(val*2);
   servo1.write(val);
-  servo2.write(-val);
+  servo2.write(180-val);
   servo3.write(val);
-  servo4.write(-val);
+  servo4.write(180-val);
+  servo5.write(val);
+  servo6.write(180-val);
+  servo7.write(val);
+  servo8.write(180-val);
+  servo9.write(val);
+  servo10.write(180-val);
+  servo11.write(val);
+  servo12.write(180-val);
   //   delay(500);
   // }
   delay(1000); // wait for a second 
